67ae18f4c15da31f89bf69422c6da4a3d3eb910c
sukanya aneja <sukanya.aneja@nytimes.com>
2021-08-19 16:25:41 -0400

    bug fixes for production docs

--- src/CameraRig.ts
+++ src/CameraRig.ts
@@ -273,7 +273,6 @@ export class CameraRig extends EventDispatcher {
 
   /**
    * Get world position and orientation of the camera
-   * @internal
    */
   getWorldCoordinates(): { position: Vector3; quaternion: Quaternion } {
     const position = new Vector3()
